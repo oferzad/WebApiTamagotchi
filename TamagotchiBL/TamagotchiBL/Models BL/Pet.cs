98bd18306af31a0d6cdc515b083e8f943774d9a7
רוני כהן <ronico612@gmail.com>
2021-01-31 15:43:00 +0200

    8

--- TamagotchiBL/TamagotchiBL/Models BL/Pet.cs
+++ TamagotchiBL/TamagotchiBL/Models BL/Pet.cs
@@ -4,14 +4,10 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
-using 
 
 namespace TamagotchiBL.Models
 {
     public partial class Pet
     {
-       
-        
-        
     }
 }
